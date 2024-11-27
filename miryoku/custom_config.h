1d16d302fef2b1d12887609dce7e2c9044f99ab9
Willy Nolan <williamnolan97@gmail.com>
2024-11-27 20:14:01 +0000

    removing extra, tap and button layers

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,3 +6,18 @@
 U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, TAB),U_LT(U_MOUSE, SPACE),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+
+#define U_EXTRA U_NUM
+#define U_TAP U_NUM
+#define U_BUTTON U_NUM
+
+#define U_BASE   0
+// #define U_EXTRA  1
+// #define U_TAP    2
+// #define U_BUTTON 3
+#define U_NAV    1
+#define U_MOUSE  2
+#define U_MEDIA  3
+#define U_NUM    4
+#define U_SYM    5
+#define U_FUN    6
